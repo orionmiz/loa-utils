68110a516232b33383524de595780ae1f9b6ba4c
orionmiz <x3ph3r@naver.com>
2022-01-31 11:33:17 +0900

    docs: fill in the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Layout from '../components/layout'
-import styles from '../styles/Home.module.css'
+import mococon from 'public/images/moco1.png'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      Home
+      <Head>
+        <title>LoaUtils</title>
+      </Head>
+      <div className='m-4'>
+        <p>
+          <Image src={mococon} /> <br />
+          기능 추가, 버그 제보 등 건의사항은 여기로 <br />
+          <Link href='https://open.kakao.com/o/saoyENWd'>
+            <a className='underline'>{'>'} 카카오톡 1:1 채팅</a>
+          </Link>
+        </p>
+      </div>
+
     </Layout>
   )
 }
