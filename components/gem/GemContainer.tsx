4fec8ec3cf609161ee639d781407a9bfacb13487
orionmiz <x3ph3r@naver.com>
2022-03-05 03:46:32 +0900

    style: use SVG instead of the text for clearing gems

--- components/gem/GemContainer.tsx
+++ components/gem/GemContainer.tsx
@@ -1,4 +1,5 @@
 import { observer } from "mobx-react-lite";
+import Image from "next/image";
 import useGem from "../../hooks/useGem";
 import { GemLists, presets } from "../../lib/gem";
 import GemList from "./GemList";
@@ -35,7 +36,9 @@ function GemContainer({ name, desc }: {
     <div className="container w-56">
       <div className="flex mb-1 items-center space-x-3">
         <span className="flex-none font-bold text-sky-700 text-xl">{desc} {' '}</span>
-        <span className="flex-none border rounded bg-gray-400 text-white font-bold cursor-pointer px-2 select-none" onClick={() => gem.clear(name)}>↺</span>
+        <span className="flex-none border rounded bg-gray-400 text-white font-bold cursor-pointer px-1 select-none" onClick={() => gem.clear(name)}>
+          <Image src="/images/redo.svg" width={16} height={16} />
+        </span>
       </div>
       <GemList gems={list} name={name} />
       <form onSubmit={onSubmit} name={name}>
